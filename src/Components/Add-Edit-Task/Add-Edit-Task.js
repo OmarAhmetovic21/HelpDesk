37e0a24a48d0886dcd1c6d7f5a934b51d35664e5
OmarAhmetovic21 <92754632+OmarAhmetovic21@users.noreply.github.com>
2024-08-05 09:18:34 +0200

    Update Add-Edit-Task.js

--- src/Components/Add-Edit-Task/Add-Edit-Task.js
+++ src/Components/Add-Edit-Task/Add-Edit-Task.js
@@ -105,43 +105,7 @@ const toggleModal = () => {
       
       </MobileView>
 
-      {/*<MobileView>
-
-      <Button style={{textAlign:"center", textDecoration:"none"}} className='button-add'
-                      onClick={toggleModal}
-                      size="lg"
-                      tag={Link}> <img style={{width:"25%", height:"100%"}} src="Plus-icon.png" alt="SarajevoGas Logo"></img>
-                      
-    </Button>
-    {modal && (
-         <div className="modal">
-          <div className="overlay"></div>
-            <div className="modal-content">
-            <Button className="button-close" onClick={toggleModal}><img style={{width:"40%", height:"40%"}} src="X-icon.png" alt="SarajevoGas Logo"></img></Button>
-            <div className="login-box" style={{marginTop:"25%"}}>
-                    <h2 style={{color:"#224798", textAlign:"center"}}>Dodajte Task</h2>
-                    <form>
-                        <label style={{color:"#224798"}} for="email">Naslov</label>
-                        <input type="email" id="email"></input>
-                        <labe style={{color:"#224798"}} for="password">Opis</labe>
-                        <input type="password" id="password"></input>
-                        <Button style={{textAlign:"center", textDecoration:"none"}} className='button' to="/dashboard"
-                      //onClick={(e) => e.preventDefault()}
-                      size="lg"
-                      tag={Link}>Sačuvaj</Button>
- 
-                    </form>
-                </div>
-            </div>
-
-   
-
-
-       </div>
-       
-          )}
-
-      </MobileView>*/}
+     
 
     </>
   );
